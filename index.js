3a30deed9aad034d20146b40e0de36ef7a2bb027
Saurav Kashyap <skashyap9934@gmail.com>
2024-12-22 14:38:05 +0530

    express config

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello, Vercel!");
 });
